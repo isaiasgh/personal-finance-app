13e908d1d1b8ea5982133956753098b6b603f81b
isaiasgh <joguherr@espol.edu.ec>
2025-05-05 21:17:11 -0500

    feat(user-service): password log entity created with JPA annotations

---
+++ user-service/src/main/java/com/isaias/finance/user_service/data/entity/PasswordLog.java
@@ -0,0 +1,26 @@
+package com.isaias.finance.user_service.data.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+public class PasswordLog {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column (nullable = false)
+    private String hashedPassword;
+
+    @Column (nullable = false)
+    private LocalDateTime attemptTimestamp;
+
+    private String loginError;
+
+    @ManyToOne
+    @JoinColumn (name = "user_id", nullable = false)
+    private User user;
+}
