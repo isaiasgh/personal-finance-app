2e1f3e5924e7088089b124cf7a151a69704f32ea
isaiasgh <joguherr@espol.edu.ec>
2025-05-06 21:50:32 -0500

    feat(user-service): implement JwtProvider, add jjwt dependency, and set EXPIRATION_TIME and SECURITY_CONSTANT in application.yaml

---
+++ user-service/src/main/java/com/isaias/finance/user_service/config/security/JwtProvider.java
@@ -0,0 +1,61 @@
+package com.isaias.finance.user_service.config.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+@Component
+public class JwtProvider {
+    @Value ("${EXPIRATION_TIME}")
+    private long EXPIRATION_TIME;
+
+    @Value ("${SECURITY_CONSTANT}")
+    private String SECURITY_CONSTANT;
+
+    SecretKey key = Keys.hmacShaKeyFor(SECURITY_CONSTANT.getBytes(StandardCharsets.UTF_8));
+
+    public String generateToken (Authentication authentication) {
+        String username = authentication.getName();
+        Date currentTime = new Date ();
+        Date expirationTime = new Date (currentTime.getTime() + EXPIRATION_TIME);
+
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(currentTime)
+                .setExpiration(expirationTime)
+                .signWith(key, SignatureAlgorithm.HS512)
+                .compact();
+    }
+
+    public String getUsernameFromJWT (String token) {
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+
+        return claims.getSubject();
+    }
+
+    public boolean validateToken (String token) {
+        try {
+            Jwts.parserBuilder()
+                    .setSigningKey(key)
+                    .build()
+                    .parseClaimsJws(token);
+
+            return true;
+        } catch (Exception ex) {
+            throw new AuthenticationCredentialsNotFoundException (ex.getMessage());
+        }
+    }
+}
