ea76a1bb73fa996ccac3cd6f91c8771b1f864a9e
isaiasgh <joguherr@espol.edu.ec>
2025-05-17 23:48:55 -0500

    feat(category-service): add Feign to call user-service for username validation

--- category-service/src/main/java/com/isaias/finance/category_service/CategoryServiceApplication.java
+++ category-service/src/main/java/com/isaias/finance/category_service/CategoryServiceApplication.java
@@ -2,8 +2,10 @@ package com.isaias.finance.category_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients(basePackages = "com.isaias.finance.category_service.domain.service")
 public class CategoryServiceApplication {
 
 	public static void main(String[] args) {
